29e325232352e1191f051cc38a9993ba6846d2aa
Muhammed Rameez <rameezmuhammed6090@gmail.com>
2025-07-01 19:37:10 +0530

    Updated Hamburger.jsx

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -19,16 +19,62 @@ const Hamburger = (props) => {
             <div>
                 <div className="text-4xl font-bold mt-14">
                     <div className="flex justify-center">
-                        <h1 className="flex gap-2 mt-4 mb-4 cursor-pointer
-                        hover:text-purple-600">
-                            <img src={insta} 
-                            className="w-8"/>
-                            <h2>
-                                Instagram
-                            </h2>
-                        </h1>
+                        <a href="https://www.instagram.com/rameezmohammed_._"
+                            target="_blank">
+                            <h1 className="flex gap-2 mt-4 mb-4 cursor-pointer
+                             hover:text-purple-600">                            
+                                <img src={insta} 
+                                className="w-8 h-9 mt-1"/>
+                                <h2>
+                                    Instagram
+                                </h2>
+                            </h1>
+                        </a>
                     </div>
-                    <div className="flex justify-center -ml-2">
+
+                    <div className="flex justify-center">
+                        <a href="https://www.facebook.com/rameez.a.rasheed.549"
+                            target="_blank">
+                            <h1 className="flex gap-2 mt-4 mb-4 cursor-pointer
+                             hover:text-purple-600">                            
+                                <img src={fb} 
+                                className="w-8 h-9 mt-1 -ml-2.5"/>
+                                <h2>
+                                    Facebook
+                                </h2>
+                            </h1>
+                        </a>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <a href="https://www.youtube.com/@rameezmohammed4932"
+                            target="_blank">
+                            <h1 className="flex gap-2 mt-4 mb-4 cursor-pointer
+                             hover:text-purple-600">                            
+                                <img src={yt} 
+                                className="w-8 h-7 mt-2 -ml-7"/>
+                                <h2>
+                                    Youtube
+                                </h2>
+                            </h1>
+                        </a>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <a href="https://www.github.com/rameezmuhammed"
+                            target="_blank">
+                            <h1 className="flex mt-4 mb-4 cursor-pointer
+                             hover:text-purple-600">                            
+                                <img src={github} 
+                                className="w-14 h-8 mt-1 -ml-14.5"/>
+                                <h2>
+                                    Github
+                                </h2>
+                            </h1>
+                        </a>
+                    </div>
+
+                    {/* <div className="flex justify-center -ml-2">
                         <h1 className="flex gap-2 mt-4 mb-4 cursor-pointer
                         hover:text-purple-600">
                             <img src={fb} 
@@ -57,7 +103,7 @@ const Hamburger = (props) => {
                                 Github
                             </h2>
                         </h1>
-                    </div>           
+                    </div>            */}
                 </div>
             </div>
 
