97f96d4c94a2352cfa95d12e6c60c9d76eee97cb
Muhammed Rameez <rameezmuhammed6090@gmail.com>
2025-07-01 10:23:32 +0530

    Updated some stuffs in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,14 +30,26 @@ const Navbar = () => {
         <div className="hidden md:block
         absolute top-0 right-0 mt-6 mr-10">
           <div className="flex gap-2">
-            <img src={insta}
-            className="w-4 h-4"/>
-            <img src={fb}
-            className="w-4 h-4"/>
-            <img src={yt}
-            className="w-6 h-4"/>
-            <img src={github}
-            className="h-4 -ml-2"/>
+            <a href="https://www.instagram.com/rameezmohammed_._"
+            target="_blank">
+              <img src={insta}
+              className="w-4 h-4"/>
+            </a>
+            <a href="https://www.facebook.com/rameez.a.rasheed.549"
+            target="_blank">
+              <img src={fb}
+              className="w-4 h-4"/>
+            </a>
+            <a href="https://www.youtube.com/@rameezmohammed4932"
+            target="_blank">
+              <img src={yt}
+              className="w-6 h-4"/>
+            </a>
+            <a href="https://www.github.com/rameezmuhammed"
+            target="_blank">
+              <img src={github}
+              className="h-4 w-9 -ml-2"/>
+            </a>              
           </div>          
         </div>
       </div>
